85659df2fe024a7c9f31c356d30b03e4f50ff0cf
techbug <techbug@naver.com>
2021-04-04 09:09:00 +0900

    Create Project

---
+++ funtions/test.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event,context){
+	return {
+		statusCode:200,
+		body: JSON.stringify({
+			name: 'RAYLEIGH',
+			age: 100,
+			email:'techbug@MediaList.com'
+		})
+	}
+}
